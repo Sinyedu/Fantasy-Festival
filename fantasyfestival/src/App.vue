a8de720ef701f3eafddee29f0bf1aa1f5917d1d6
NimNim20 <benvio01@easv365.dk>
2024-05-14 10:05:28 +0200

    File renaming

--- fantasyfestival/src/App.vue
+++ fantasyfestival/src/App.vue
@@ -8,8 +8,8 @@ import { RouterLink, RouterView } from 'vue-router'
     <div class="wrapper">
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Quest</RouterLink>
+        <RouterLink to="/familie">Familie Quest</RouterLink>
       </nav>
     </div>
   </header>
--- fantasyfestival/src/router/index.js
+++ fantasyfestival/src/router/index.js
@@ -1,21 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import QuestView from '../views/QuestView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'quest',
+      component: QuestView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/familie',
+      name: 'familie',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/FamilieView.vue')
     }
   ]
 })
