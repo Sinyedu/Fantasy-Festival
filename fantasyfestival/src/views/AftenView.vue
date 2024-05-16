3741707136cd9cbe4e52be1d560b1e0b5ea7dcd9
Benjamin Vid Gard <benvio01@easv365.dk>
2024-05-16 16:48:17 +0200

    Added body tag to the page

--- fantasyfestival/src/views/AftenView.vue
+++ fantasyfestival/src/views/AftenView.vue
@@ -1,68 +1,70 @@
 <template>
-  <div class="herocontainer">
-    <div class="hero">
-      <img class="gnist" src="../assets/img/dragonnight.svg" alt="Dragen Gnist">      
+  <body>
+    <div class="herocontainer">
+      <div class="hero">
+        <img class="gnist" src="../assets/img/dragonnight.svg" alt="Dragen Gnist">      
+      </div>
+      <div class="hands">
+        <img src="../assets/img/dragonhands.svg" alt="Gnist's hænder">
+      </div>
     </div>
-    <div class="hands">
-      <img src="../assets/img/dragonhands.svg" alt="Gnist's hænder">
+  
+    <div class="titlecontainer">
+      <div class="centertext">
+        <h1 class="font-bold text-3xl text-center"> Aften Questen!</h1>
+      </div>
     </div>
-  </div>
-
-  <div class="titlecontainer">
-    <div class="centertext">
-      <h1 class="font-bold text-3xl text-center"> Aften Questen!</h1>
-    </div>
-  </div>
-
-  <div class="questcontainer">
-    <div class="infotext">
-      <h1 class="font-bold text-3xl pb-9">Er du til uhygge og svære gåder?</h1>
-      <p class="text-2xl">
-        Når tusmørket begynder at sænke sig over Esbjerg, skrues sværhedsgraden i AftenQuesten op. 
-        <br><br>
-        Nu skal der virkelig tænkes kreativt og løses opgaver, der kræver både mod, samarbejde og snilde for at I kan gennemføre questen og redde Esbjerg.
-        <br><br>
-        Er du vild med at løse gåder og knække koder, så må du ikke snyde dig selv for denne ultimative oplevelse.
-        <br><br>
-        Vi sender dig og vennerne ud på jeres ultimative eventyr i Esbjergs gader og stræder. Her vil I blive nødt til at samarbejde og udtænke nye løsninger for at gennemføre AftenQuesten.
-      </p>
-    </div>
-
-    <div class="book-button">
-    </div>
-    
-    <!-- Dynamically retrieves the general information about the night quest -->
-    <div class="container-card">
-      <a href="https://www.universe.com/events/fantasy-quest-aftenquest-tickets-2PCHR4" target="_blank">
-        <div class="book">
-          <img class="ylwbutton" src="../assets/img/yellow butten.svg" alt="Yellow Button">
-        </div>
-      </a>
-
-      <div v-for="infoDetail in infoDetails" :key="infoDetail.id" class="card">
-        <div class="info-data">
-          <div class="info">
-            <h1 class="titletext">{{ infoDetail.title }}</h1>
-            <h2 class="date">{{ infoDetail.date }}</h2>
-            <h2 class="time">{{ infoDetail.time }}</h2>
-            <h2 class="location">{{ infoDetail.location }}</h2>
-            <h2 class="duration">{{ infoDetail.duration }}</h2>
-            <h2 class="ages">{{ infoDetail.ages }}</h2>
+  
+    <div class="questcontainer">
+      <div class="infotext">
+        <h1 class="font-bold text-3xl pb-9">Er du til uhygge og svære gåder?</h1>
+        <p class="text-2xl">
+          Når tusmørket begynder at sænke sig over Esbjerg, skrues sværhedsgraden i AftenQuesten op. 
+          <br><br>
+          Nu skal der virkelig tænkes kreativt og løses opgaver, der kræver både mod, samarbejde og snilde for at I kan gennemføre questen og redde Esbjerg.
+          <br><br>
+          Er du vild med at løse gåder og knække koder, så må du ikke snyde dig selv for denne ultimative oplevelse.
+          <br><br>
+          Vi sender dig og vennerne ud på jeres ultimative eventyr i Esbjergs gader og stræder. Her vil I blive nødt til at samarbejde og udtænke nye løsninger for at gennemføre AftenQuesten.
+        </p>
+      </div>
+  
+      <div class="book-button">
+      </div>
+      
+      <!-- Dynamically retrieves the general information about the night quest -->
+      <div class="container-card">
+        <a href="https://www.universe.com/events/fantasy-quest-aftenquest-tickets-2PCHR4" target="_blank">
+          <div class="book">
+            <img class="ylwbutton" src="../assets/img/yellow butten.svg" alt="Yellow Button">
+          </div>
+        </a>
+  
+        <div v-for="infoDetail in infoDetails" :key="infoDetail.id" class="card">
+          <div class="info-data">
+            <div class="info">
+              <h1 class="titletext">{{ infoDetail.title }}</h1>
+              <h2 class="date">{{ infoDetail.date }}</h2>
+              <h2 class="time">{{ infoDetail.time }}</h2>
+              <h2 class="location">{{ infoDetail.location }}</h2>
+              <h2 class="duration">{{ infoDetail.duration }}</h2>
+              <h2 class="ages">{{ infoDetail.ages }}</h2>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-
-  <div class="video-holder">
-    <img class="holder" src="../assets/img/dragonholder.png" alt="Dragon Tail">
-    <iframe class="video" width="1280" height="600" src="https://www.youtube.com/embed/AFBSUQvmt1o" title="Commercial - Red Merrild (For your perfect morning)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-  </div>
-
-
-  <div class="footer-placeholder bg-blue-900">
-
-  </div>
+  
+    <div class="video-holder">
+      <img class="holder" src="../assets/img/dragonholder.png" alt="Dragon Tail">
+      <iframe class="video" width="1280" height="600" src="https://www.youtube.com/embed/AFBSUQvmt1o" title="Commercial - Red Merrild (For your perfect morning)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+  
+  
+    <div class="footer-placeholder bg-blue-900">
+  
+    </div>
+  </body>
 
 </template>
 
@@ -73,6 +75,14 @@
 </script>
 
 <style lang="scss" scoped>
+body{
+  margin: 0;
+  padding: 0;
+  font-family: 'Poppins', sans-serif;
+  background-color: green;
+
+}
+
 .herocontainer{
   // overflow: hidden;
   position: relative;
@@ -151,6 +161,7 @@
   justify-content: center;
   box-shadow: 0 0 25px rgba(12, 0, 80, 0.3);
   border-radius: 7%;
+  background-color: white;
 }
 
 .card h1 {
