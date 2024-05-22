c2c6277f3ebdf4fed0b48bf0a88e53fc8a2f6952
Benjamin Vid Gard <benvio01@easv365.dk>
2024-05-22 14:33:39 +0200

    Fixed most the content to fit

--- fantasyfestival/src/views/FamilieView.vue
+++ fantasyfestival/src/views/FamilieView.vue
@@ -1,102 +1,150 @@
-
 <template>
-  <div class="herocontainer">
-    <div class="hero">
-      <img class="" src="../assets/img/dragonnight.png" alt="Dragen Gnist">      
+  <body>
+    <div class="herocontainer">
+      <div class="hero">
+        <img class="gnist" src="../assets/img/dragonnight.svg" alt="Dragen Gnist">      
+      </div>
+      <div class="hands">
+        <img src="../assets/img/lefthand.png" alt="Gnist's venstre hånd">
+        <img src="../assets/img/righthand.png" alt="Gnist's venstre hånd">
+      </div>
     </div>
-    <div class="hands">
-      <img src="../assets/img/dragonhands.png" alt="Gnist's hænder">
+  
+    <div class="titlecontainer">
+      <div class="centertext">
+        <h1 class="headliner font-bold text-3xl text-center">Familie Questen!</h1>
+      </div>
     </div>
-  </div>
-
-  <div class="titlecontainer">
-    <div class="centertext">
-      <h1 class="font-bold text-3xl text-center"> Familie Quest!</h1>
-    </div>
-  </div>
-
-  <div class="questcontainer">
-    <div class="infotext">
-      <h1 class="font-bold text-3xl pb-9">Er du til uhygge og svære gåder?</h1>
-      <p class="text-2xl">
-        Når tusmørket begynder at sænke sig over Esbjerg, skrues sværhedsgraden i AftenQuesten op. 
-        <br><br>
-        Nu skal der virkelig tænkes kreativt og løses opgaver, der kræver både mod, samarbejde og snilde for at I kan gennemføre questen og redde Esbjerg.
-        <br><br>
-        Er du vild med at løse gåder og knække koder, så må du ikke snyde dig selv for denne ultimative oplevelse.
-        <br><br>
-        Vi sender dig og vennerne ud på jeres ultimative eventyr i Esbjergs gader og stræder. Her vil I blive nødt til at samarbejde og udtænke nye løsninger for at gennemføre AftenQuesten.
-      </p>
-    </div>
-
-    <div class="book-button">
-    </div>
-    
-    <!-- Dynamically retrieves the general information about the night quest -->
-    <div class="container-card">
-      <a href="https://www.universe.com/events/fantasy-quest-aftenquest-tickets-2PCHR4" target="_blank">
-        <button class="button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          BOOK NU
-        </button>
-      </a>
-
-      <div v-for="dayDetail in dayDetails" :key="dayDetail.id" class="card">
-        <div class="info-data">
-          <div class="info">
-            <h1 class="titletext">{{ dayDetail.title }}</h1>
-            <h2 class="date">{{ dayDetail.date }}</h2>
-            <h2 class="time">{{ dayDetail.time }}</h2>
-            <h2 class="location">{{ dayDetail.location }}</h2>
-            <h2 class="duration">{{ dayDetail.duration }}</h2>
-            <h2 class="ages">{{ dayDetail.ages }}</h2>
+  
+    <div class="questcontainer">
+      <div class="infotext">
+        <h1 class="headliner font-bold text-3xl pb-9">Deltag i en eventyrlig oplevelse</h1>
+        <p class="maintext text-2xl">
+          Familieudgaven af Fantasy Quest Esbjerg er et oplevelsesløb for hele familien, med tryk på oplevelse. For det er ikke et klassisk løb. Det handler om at leve sig ind i en anden verden, en magisk historie, hvor I sammen er på en ”quest” – en mission, en opgave, en jagt.
+          <br><br>
+          Her møder I magiske væsner, der stiller jer nogle opgaver og udfordringer undervejs mod målet, som I skal løse sammen for at gennemføre Questen.
+          <br><br>
+          Sværhedsgraden er sænket, så børnene kan være med til at løse opgaverne. Men tag ikke fejl, der er brug for alle gode kræfter og samarbejdsevner, hvis I skal være med til at redde Esbjerg.
+          <br><br>
+          Som en ekstra bonus kan I møde YouTuberen Lakserytteren undervejs i Questen rundt i Esbjergs gader.
+        </p>
+      </div>
+  
+      
+      <!-- Dynamically retrieves the general information about the evening quest -->
+      <div class="container-card">
+        <a href="https://www.universe.com/events/fantasy-quest-aftenquest-tickets-2PCHR4" target="_blank">
+          <div class="book">
+            <img class="ylwbutton" src="../assets/img/bookbutton.svg" alt="Yellow Button">
+            <h1 class="book-text">Book billetter</h1>
+          </div>
+        </a>
+  
+        <div v-for="infoDetail in infoDetails" :key="infoDetail.id" class="card">
+          <div class="info-data">
+            <div class="info">
+              <h1 class="titletext">{{ infoDetail.title }}</h1>
+              <h2 class="date">{{ infoDetail.date }}</h2>
+              <h2 class="time">{{ infoDetail.time }}</h2>
+              <h2 class="location">{{ infoDetail.location }}</h2>
+              <h2 class="duration">{{ infoDetail.duration }}</h2>
+              <h2 class="ages">{{ infoDetail.ages }}</h2>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
 
-  <div class="video-holder">
-    <img class="holder" src="../assets/img/dragonholder.png" alt="Dragon Tail">
-    <iframe class="video" width="1280" height="600" src="https://www.youtube.com/embed/AFBSUQvmt1o" title="Commercial - Red Merrild (For your perfect morning)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-  </div>
+    <!-- Video trailer for the evening quest -->
+    <div class="video-holder">
+      <img class="top-half" src="../assets/img/toptail.png" alt="Toppen af Gnist's hale">
+      <iframe class="video" width="1280" height="700" src="https://www.youtube.com/embed/AFBSUQvmt1o" title="Commercial - Red Merrild (For your perfect morning)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <img class="bottom-half" src="../assets/img/bottomtail.png" alt="Bunden af Gnist's hale">
+    </div>
 
-
-  <div class="footer-placeholder bg-blue-900">
-
-  </div>
-
+    <div class="questcontainer">
+      <div class="infotext">
+        <h1 class="headliner font-bold text-3xl pb-9">I gamle dage sagde man...</h1>
+        <p class="maintext text-2xl">
+          ...at det onde ville komme fra havet. Gennem generationer blev den fortælling givet videre. Man hørte den, da Esbjerg blev grundlagt i 1868. Man holdt altid øje med havet. Mod vest. 
+        </p>
+      </div>
+      <img src="#" alt="Eventyr Goblin">
+    </div>
+  
+    <div class="footer-placeholder bg-blue-900">
+  
+    </div>
+  </body>
 
 </template>
 
-
 <script setup>
   import { ref } from "vue";
-  import getDayDetails from '@/modules/getDayInfo.js';
-  const { dayDetails } = getDayDetails();
+  import getInfoDetails from '@/modules/getInfo.js';
+  const { infoDetails } = getInfoDetails();
 </script>
 
 <style lang="scss" scoped>
-.herocontainer{
-  display: flex;
-  width: 100%;
-  justify-content: center;
+body{
+  margin: 0;
+  padding: 0;
+  background-color: #355C55;
+}
+.info {
+  color: #EDE6D6;
 }
 
-.hero{
+
+.maintext {
+color: #D0D3C9;
+}
+.headliner {
+  color: #FECD08;
+}
+
+.herocontainer{
+  position: relative;
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+
+.hero {
+  overflow: hidden;
   display: flex;
   justify-content: center;
-  height: 101vh;
+  align-items: center; 
+  text-align: center;
+  position: relative;
+  width: 100%; 
+  height: 100%; 
+
+
+}
+
+.gnist {
+  overflow: hidden;
+  position: relative;
+  display: flex;
+  max-width: 100%; 
+  max-height: 100%; 
+  scale: 2.40;
 }
 
 .hands{
   display: flex;
   justify-content: center;
-  align-items: center;
   position: absolute;
   width: 100%;
-  // height: 100vh;
-  margin-top: 33%;
-  z-index: 2;
+  height: 100vh;
+  margin-top: 45%;
+  z-index: 100;
+  scale: 0.6;
+  gap: 77%;
 }
 
 .questcontainer{
@@ -131,8 +179,6 @@
   padding-top: 3%;
   align-items: center; 
   justify-content: center;
-  box-shadow: 0 0 25px rgba(12, 0, 80, 0.3);
-  border-radius: 7%;
 }
 
 .card h1 {
@@ -159,6 +205,15 @@
   font-size: 2rem;
 }
 
+.book-text{
+  display: flex;
+  color: #EDE6D6;
+  font-size: 1.3rem;
+  margin-left: 2%;
+  margin-bottom: 10%;
+  position: absolute;
+}
+
 a{
   text-decoration: none;
   width: 33%;
@@ -170,25 +225,38 @@ a{
   height: 100vh;
   justify-content: center;
   position: relative;
-  margin-top: 10%;
-  margin-bottom: 10%;
+  margin-top: 25%;
+  margin-bottom: 25%;
   z-index: 0;
 }
 
-.holder{
+.top-half{
   position: absolute;
   display: flex;
-  width: auto;
+  width: 100%;
   height: 100%;
-  margin-left: 42%;
-  scale: 1.20;
+  left: 23%;
+  bottom: 43.9%;
+  scale: 0.6;
   z-index: 2;
 }
 
 .video{
   display: flex;
   margin-top: 17.9%;
-  z-index: 1;
+  z-index: 3;
+}
+
+.bottom-half{
+  position: absolute;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  left: 7%;
+  top: 56.3%;
+  scale: 0.4;
+  z-index: 4;
+  pointer-events: none;
 }
 
 .footer-placeholder{
