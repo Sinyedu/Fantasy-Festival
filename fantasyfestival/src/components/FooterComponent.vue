f47898799ac968910f8c99290d7f7ed857d85abe
Sinyedu <simnyb01@easv365.dk>
2024-05-23 15:06:36 +0200

    FooterComponent

--- fantasyfestival/src/components/FooterComponent.vue
+++ fantasyfestival/src/components/FooterComponent.vue
@@ -1,23 +1,23 @@
 <template>
     <div class="masterfooter">
-      <footer class="footer raleway-font bg-teal-900 text-cream p-5 h-92">
-        <div class="footer-content flex justify-between items-center max-w-6xl mx-auto">
-          <div class="footer-column flex-1 p-2">
-            <ul class="footer-list list-none p-0">
-              <li class="footer-item py-1">Køb billetter</li>
-              <li class="footer-item py-1">Familie Quest</li>
-              <li class="footer-item py-1">Aften Quest</li>
+      <footer class="footer raleway-font">
+        <div class="footer-content">
+          <div class="footer-column">
+            <ul class="footer-list">
+              <li class="footer-item1">Køb billetter</li>
+              <li class="footer-item1">Familie Quest</li>
+              <li class="footer-item1">Aften Quest</li>
             </ul>
           </div>
-          <div class="footer-column flex-1 p-2">
-            <ul class="footer-list list-none p-0">
-              <li class="footer-item py-1">FAQ</li>
-              <li class="footer-item py-1">Kontakt os</li>
-              <li class="footer-item py-1">Link til Fantasyfestivalen</li>
+          <div class="footer-column">
+            <ul class="footer-list">
+              <li class="footer-item2">FAQ</li>
+              <li class="footer-item2">Kontakt os</li>
+              <li class="footer-item2">Link til Fantasyfestivalen</li>
             </ul>
           </div>
-          <div class="footer-column footer-logo flex-1 p-2 flex justify-end">
-            <img src="../assets/img/logogultekst.png" alt="Logo" class="max-w-24">
+          <div class="footer-column footer-logo">
+            <img src="../assets/img/logogultekst.png" alt="Logo" class="logo-img">
           </div>
         </div>
       </footer>
@@ -31,40 +31,56 @@
   </script>
   
   <style scoped lang="scss">
-
-.footer-item {
-    display: flex;
-    justify-content: start;
-}
-
-
-.footer-column {
-    display: flex;
-    flex-direction: row;
-    /* justify-content: start; */
-}
-
-
-.footer-column img {
-    scale: 3.3;
-}
-
-  li {
-    display: flex;
-    justify-content: start;
-    font-size: 1.5rem;
+  .masterfooter {
+    width: 100%;
   }
   
-  .bg-teal-900 {
+  .footer {
     background-color: #083A35;
-  }
-  
-  .text-cream {
     color: #ede6d6;
+    padding-top: 2%;
+    padding-left: 50px;
   }
   
-  .max-w-24 {
-    max-width: 100px;
+  .footer-content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+  }
+  
+  .footer-column {
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+  }
+  
+  .footer-list {
+    list-style: none;
+    padding: 0;
+  }
+  
+  .footer-item1 {
+    font-size: 1.5rem;
+    margin-bottom: 10px;
+  }
+  .footer-item2 {
+    font-size: 1.5rem;
+    margin-bottom: 10px;
+    display: flex;
+    justify-content: start;
+    position: relative;
+    left: -80%;
+  }
+  
+  .footer-logo {
+    display: flex;
+    justify-content: end;
+    padding-right: 2.5%;
+  }
+  
+  .logo-img {
+    max-width: 350px;
   }
   </style>
   
