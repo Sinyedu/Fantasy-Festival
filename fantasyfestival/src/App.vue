fa950e8884252e99bdfa284b2f0a47681def3264
NimNim20 <benvio01@easv365.dk>
2024-05-29 13:39:29 +0200

    Removed block comments

--- fantasyfestival/src/App.vue
+++ fantasyfestival/src/App.vue
@@ -11,16 +11,8 @@
   <Footer />
 </template>
 
-<!-- <script
-  src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.2/anime.min.js"
-  integrity="sha512-aNMyYYxdIxIaot0Y1/PLuEu3eipGCmsEUBrUq+7aVyPGMFH8z0eTP0tkqAvv34fzN6z+201d3T8HPb1svWSKHQ=="
-  crossorigin="anonymous"
-  referrerpolicy="no-referrer"
-></script> -->
-
 <script setup>
-import { ref } from "vue";
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 import NavBar from "./components/NavBar.vue";
 import Footer from "./components/FooterComponent.vue";
 import ScrollToTop from "./components/ScrollToTop.vue";
